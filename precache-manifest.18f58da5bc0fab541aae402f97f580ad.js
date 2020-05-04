b4cfa7080464c2b46610666a2a48f747d16c7113
Anton Kurdo <anton.kurdo@gmail.com>
2020-05-04 13:32:20 +0300

    Updates

---
+++ precache-manifest.18f58da5bc0fab541aae402f97f580ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c972561a27567572023dd9cd500de3",
+    "url": "/todo-react-app/index.html"
+  },
+  {
+    "revision": "343c78215e6ece78283b",
+    "url": "/todo-react-app/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "554467be4aa7a0662a29",
+    "url": "/todo-react-app/static/css/main.7c4cd713.chunk.css"
+  },
+  {
+    "revision": "343c78215e6ece78283b",
+    "url": "/todo-react-app/static/js/2.9cddcacf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react-app/static/js/2.9cddcacf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "554467be4aa7a0662a29",
+    "url": "/todo-react-app/static/js/main.2617afb6.chunk.js"
+  },
+  {
+    "revision": "dd3e2bd57492334dda3e",
+    "url": "/todo-react-app/static/js/runtime-main.eff59e80.js"
+  }
+]);
